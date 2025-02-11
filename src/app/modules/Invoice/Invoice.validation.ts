ca7511a0a30f09e2bd1b9b0777b3ee924800767a
Ahmad <ahmadmusa9805@gmail.com>
2025-02-11 11:17:35 +0600

    add some part

--- src/app/modules/Invoice/Invoice.validation.ts
+++ src/app/modules/Invoice/Invoice.validation.ts
@@ -5,9 +5,12 @@ export const createInvoiceValidationSchema = z.object({
   body: z.object({
     Invoice: z.object({
       jobId: z.string().min(1),
-      clientAdminName: z.string().optional(),
-      serviceName: z.string().min(1),
-      servicePrice: z.string().min(1),
+      clientAdminName: z.string().min(1), // Ensure at least one admin().optional(),
+      services: z.object({
+        serviceName: z.string().min(1, "Service name is required"),
+        serviceCost: z.number().positive("Service cost must be a positive number"),
+        quantity: z.number().int().positive("Quantity must be a positive integer"),
+      }).array(),
       paymentStatus: z.string().min(1),
       totalCost: z.string().min(1),
       isDeleted: z.boolean().default(false),
